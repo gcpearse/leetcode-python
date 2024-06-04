9a779844fad0ed446b4089de271f74818522e2bb
gcpearse <glen.c.pearse@gmail.com>
2024-06-04 19:13:52 +0100

    adds tested solution

---
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,33 @@
+def max_profit(prices: list[int]) -> int:
+  
+  profit = 0
+  smallest = prices[0]
+
+  for i in range(1, len(prices)):
+
+    current = prices[i]
+
+    if current < smallest:
+      smallest = current
+    if current - smallest > profit:
+      profit = current - smallest
+
+  return profit
+
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ 
+# Example 1:
+
+# Input: prices = [7, 1, 5, 3, 6, 4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+# Example 2:
+
+# Input: prices = [7, 6, 4, 3, 1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit = 0.
