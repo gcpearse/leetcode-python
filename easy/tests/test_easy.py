414722c0a25ec4f1eb6635facfaf250b2fe53a6c
gcpearse <glen.c.pearse@gmail.com>
2024-06-18 14:55:55 +0100

    testing pipeline

--- easy/tests/test_easy.py
+++ easy/tests/test_easy.py
@@ -138,4 +138,4 @@ def test_add_digits():
 def test_missing_number():
   assert missing_number.Solution().missing_number([3, 0, 1]) == 2
   assert missing_number.Solution().missing_number([0, 1]) == 2
-  assert missing_number.Solution().missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
+  assert missing_number.Solution().missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 7
