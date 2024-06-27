8f261458f08ee26416c80367ba639b11bb054a03
gcpearse <glen.c.pearse@gmail.com>
2024-06-27 18:19:34 +0100

    refactors test suite

--- easy/tests/test_easy.py
+++ easy/tests/test_easy.py
@@ -24,141 +24,164 @@ import power_of_four
 
 
 def test_two_sum():
-  assert two_sum.Solution().two_sum([2, 7, 11, 15], 9) == [0, 1]
-  assert two_sum.Solution().two_sum([3, 2, 4], 6) == [1, 2]
-  assert two_sum.Solution().two_sum([3, 3], 6) == [0, 1]
+  solution = two_sum.Solution()
+  assert solution.two_sum([2, 7, 11, 15], 9) == [0, 1]
+  assert solution.two_sum([3, 2, 4], 6) == [1, 2]
+  assert solution.two_sum([3, 3], 6) == [0, 1]
 
 
 def test_is_palindrome():
-  assert palindrome_number.Solution().is_palindrome(121) == True
-  assert palindrome_number.Solution().is_palindrome(-121) == False
-  assert palindrome_number.Solution().is_palindrome(10) == False
+  solution = palindrome_number.Solution()
+  assert solution.is_palindrome(121) == True
+  assert solution.is_palindrome(-121) == False
+  assert solution.is_palindrome(10) == False
 
 
 def test_roman_to_int():
-  assert roman_to_integer.Solution().roman_to_int("III") == 3
-  assert roman_to_integer.Solution().roman_to_int("LVIII") == 58
-  assert roman_to_integer.Solution().roman_to_int("MCMXCIV") == 1994
+  solution = roman_to_integer.Solution()
+  assert solution.roman_to_int("III") == 3
+  assert solution.roman_to_int("LVIII") == 58
+  assert solution.roman_to_int("MCMXCIV") == 1994
 
 
 def test_longest_common_prefix():
-  assert longest_common_prefix.Solution().longest_common_prefix(["flower", "flow", "flight"]) == "fl"
-  assert longest_common_prefix.Solution().longest_common_prefix(["dog", "racecar", "car"]) == ""
-  assert longest_common_prefix.Solution().longest_common_prefix(["cir", "car"]) == "c"
+  solution = longest_common_prefix.Solution()
+  assert solution.longest_common_prefix(["flower", "flow", "flight"]) == "fl"
+  assert solution.longest_common_prefix(["dog", "racecar", "car"]) == ""
+  assert solution.longest_common_prefix(["cir", "car"]) == "c"
 
 
 def test_is_valid():
-  assert valid_parentheses.Solution().is_valid("()") == True
-  assert valid_parentheses.Solution().is_valid("()[]{}") == True
-  assert valid_parentheses.Solution().is_valid("(]") == False
-  assert valid_parentheses.Solution().is_valid("]") == False
-  assert valid_parentheses.Solution().is_valid("){") == False
-  assert valid_parentheses.Solution().is_valid("([}}])") == False
+  solution = valid_parentheses.Solution()
+  assert solution.is_valid("()") == True
+  assert solution.is_valid("()[]{}") == True
+  assert solution.is_valid("(]") == False
+  assert solution.is_valid("]") == False
+  assert solution.is_valid("){") == False
+  assert solution.is_valid("([}}])") == False
 
 
 def test_str_str():
-  assert find_the_index_of_the_first_occurrence_in_a_string.Solution().str_str("sadbutsad", "sad") == 0
-  assert find_the_index_of_the_first_occurrence_in_a_string.Solution().str_str("leetcode", "leeto") == -1
+  solution = find_the_index_of_the_first_occurrence_in_a_string.Solution()
+  assert solution.str_str("sadbutsad", "sad") == 0
+  assert solution.str_str("leetcode", "leeto") == -1
 
 
 def test_search_insert():
-  assert search_input_position.Solution().search_insert([1, 3, 5, 6], 5) == 2
-  assert search_input_position.Solution().search_insert([1, 3, 5, 6], 2) == 1
-  assert search_input_position.Solution().search_insert([1, 3, 5, 6], 7) == 4
-  assert search_input_position.Solution().search_insert([1], 1) == 0
+  solution = search_input_position.Solution()
+  assert solution.search_insert([1, 3, 5, 6], 5) == 2
+  assert solution.search_insert([1, 3, 5, 6], 2) == 1
+  assert solution.search_insert([1, 3, 5, 6], 7) == 4
+  assert solution.search_insert([1], 1) == 0
 
 
 def test_length_of_last_word():
-  assert length_of_last_word.Solution().length_of_last_word("Hello World") == 5
-  assert length_of_last_word.Solution().length_of_last_word("   fly me   to   the moon  ") == 4
-  assert length_of_last_word.Solution().length_of_last_word("luffy is still joyboy") == 6
-  assert length_of_last_word.Solution().length_of_last_word("a") == 1
+  solution = length_of_last_word.Solution()
+  assert solution.length_of_last_word("Hello World") == 5
+  assert solution.length_of_last_word("   fly me   to   the moon  ") == 4
+  assert solution.length_of_last_word("luffy is still joyboy") == 6
+  assert solution.length_of_last_word("a") == 1
 
 
 def test_plus_one():
-  assert plus_one.Solution().plus_one([1, 2, 3]) == [1, 2, 4]
-  assert plus_one.Solution().plus_one([4, 3, 2, 1]) == [4, 3, 2, 2]
-  assert plus_one.Solution().plus_one([9]) == [1, 0]
+  solution = plus_one.Solution()
+  assert solution.plus_one([1, 2, 3]) == [1, 2, 4]
+  assert solution.plus_one([4, 3, 2, 1]) == [4, 3, 2, 2]
+  assert solution.plus_one([9]) == [1, 0]
 
 
 def test_max_profit():
-  assert best_time_to_buy_and_sell_stock.Solution().max_profit([7, 1, 5, 3, 6, 4]) == 5
-  assert best_time_to_buy_and_sell_stock.Solution().max_profit([7, 6, 4, 3, 1]) == 0
+  solution = best_time_to_buy_and_sell_stock.Solution()
+  assert solution.max_profit([7, 1, 5, 3, 6, 4]) == 5
+  assert solution.max_profit([7, 6, 4, 3, 1]) == 0
 
 
 def test_is_valid_palindrome():
-  assert valid_palindrome.Solution().is_valid_palindrome("A man, a plan, a canal: Panama") == True
-  assert valid_palindrome.Solution().is_valid_palindrome("race a car") == False
-  assert valid_palindrome.Solution().is_valid_palindrome(" ") == True
-  assert valid_palindrome.Solution().is_valid_palindrome("ab_a") == True
+  solution = valid_palindrome.Solution()
+  assert solution.is_valid_palindrome("A man, a plan, a canal: Panama") == True
+  assert solution.is_valid_palindrome("race a car") == False
+  assert solution.is_valid_palindrome(" ") == True
+  assert solution.is_valid_palindrome("ab_a") == True
 
 
 def test_single_number():
-  assert single_number.Solution().single_number([2, 2, 1]) == 1
-  assert single_number.Solution().single_number([4, 1, 2, 1, 2]) == 4
-  assert single_number.Solution().single_number([1]) == 1
+  solution = single_number.Solution()
+  assert solution.single_number([2, 2, 1]) == 1
+  assert solution.single_number([4, 1, 2, 1, 2]) == 4
+  assert solution.single_number([1]) == 1
 
 
 def test_convert_to_title():
-  assert excel_sheet_column_title.Solution().convert_to_title(1) == "A"
-  assert excel_sheet_column_title.Solution().convert_to_title(28) == "AB"
-  assert excel_sheet_column_title.Solution().convert_to_title(701) == "ZY"
+  solution = excel_sheet_column_title.Solution()
+  assert solution.convert_to_title(1) == "A"
+  assert solution.convert_to_title(28) == "AB"
+  assert solution.convert_to_title(701) == "ZY"
 
 
 def test_majority_element():
-  assert majority_element.Solution().majority_element([3, 2, 3]) == 3
-  assert majority_element.Solution().majority_element([2, 2, 1, 1, 1, 2, 2]) == 2
+  solution = majority_element.Solution()
+  assert solution.majority_element([3, 2, 3]) == 3
+  assert solution.majority_element([2, 2, 1, 1, 1, 2, 2]) == 2
 
 
 def test_title_to_number():
-  assert excel_sheet_column_number.Solution().title_to_number("A") == 1
-  assert excel_sheet_column_number.Solution().title_to_number("AB") == 28
-  assert excel_sheet_column_number.Solution().title_to_number("ZY") == 701
+  solution = excel_sheet_column_number.Solution()
+  assert solution.title_to_number("A") == 1
+  assert solution.title_to_number("AB") == 28
+  assert solution.title_to_number("ZY") == 701
 
 
 def test_contains_duplicate():
-  assert contains_duplicate.Solution().contains_duplicate([1, 2, 3, 1]) == True
-  assert contains_duplicate.Solution().contains_duplicate([1, 2, 3, 4]) == False
-  assert contains_duplicate.Solution().contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
+  solution = contains_duplicate.Solution()
+  assert solution.contains_duplicate([1, 2, 3, 1]) == True
+  assert solution.contains_duplicate([1, 2, 3, 4]) == False
+  assert solution.contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
 
 
 def test_is_power_of_two():
-  assert power_of_two.Solution().is_power_of_two(1) == True
-  assert power_of_two.Solution().is_power_of_two(16) == True
-  assert power_of_two.Solution().is_power_of_two(3) == False
+  solution = power_of_two.Solution()
+  assert solution.is_power_of_two(1) == True
+  assert solution.is_power_of_two(16) == True
+  assert solution.is_power_of_two(3) == False
 
 
 def test_is_anagram():
-  assert valid_anagram.Solution().is_anagram("anagram", "nagaram") == True
-  assert valid_anagram.Solution().is_anagram("rat", "car") == False
+  solution = valid_anagram.Solution()
+  assert solution.is_anagram("anagram", "nagaram") == True
+  assert solution.is_anagram("rat", "car") == False
 
 
 def test_add_digits():
-  assert add_digits.Solution().add_digits(38) == 2
-  assert add_digits.Solution().add_digits(0) == 0
+  solution = add_digits.Solution()
+  assert solution.add_digits(38) == 2
+  assert solution.add_digits(0) == 0
 
 
 def test_missing_number():
-  assert missing_number.Solution().missing_number([3, 0, 1]) == 2
-  assert missing_number.Solution().missing_number([0, 1]) == 2
-  assert missing_number.Solution().missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
+  solution = missing_number.Solution()
+  assert solution.missing_number([3, 0, 1]) == 2
+  assert solution.missing_number([0, 1]) == 2
+  assert solution.missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
 
 
 def test_word_pattern():
-  assert word_pattern.Solution().word_pattern("abba", "dog cat cat dog") == True
-  assert word_pattern.Solution().word_pattern("abba", "dog cat cat fish") == False
-  assert word_pattern.Solution().word_pattern("aaaa", "dog cat cat dog") == False
-  assert word_pattern.Solution().word_pattern("abba", "dog dog dog dog") == False
+  solution = word_pattern.Solution()
+  assert solution.word_pattern("abba", "dog cat cat dog") == True
+  assert solution.word_pattern("abba", "dog cat cat fish") == False
+  assert solution.word_pattern("aaaa", "dog cat cat dog") == False
+  assert solution.word_pattern("abba", "dog dog dog dog") == False
 
 
 def test_is_power_of_three():
-  assert power_of_three.Solution().is_power_of_three(27) == True
-  assert power_of_three.Solution().is_power_of_three(0) == False
-  assert power_of_three.Solution().is_power_of_three(-1) == False
-  assert power_of_three.Solution().is_power_of_three(9) == True
+  solution = power_of_three.Solution()
+  assert solution.is_power_of_three(27) == True
+  assert solution.is_power_of_three(0) == False
+  assert solution.is_power_of_three(-1) == False
+  assert solution.is_power_of_three(9) == True
 
 
 def test_is_power_of_four():
-  assert power_of_four.Solution().is_power_of_four(16) == True
-  assert power_of_four.Solution().is_power_of_four(5) == False
-  assert power_of_four.Solution().is_power_of_four(1) == True
+  solution = power_of_four.Solution()
+  assert solution.is_power_of_four(16) == True
+  assert solution.is_power_of_four(5) == False
+  assert solution.is_power_of_four(1) == True
