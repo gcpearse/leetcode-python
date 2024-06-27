c25da6b45e4f44fd233a2e554cb6760bc3737366
gcpearse <glen.c.pearse@gmail.com>
2024-06-27 18:05:39 +0100

    adds tested solution

---
+++ easy/power_of_four.py
@@ -0,0 +1,32 @@
+class Solution:
+  def is_power_of_four(self, n: int) -> bool:
+    
+    i = 0
+
+    while True:
+      if 4 ** i <= n:
+        if 4 ** i == n:
+          return True
+      else:
+        return False
+      i += 1
+
+
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+# An integer n is a power of four, if there exists an integer x such that n == 4^x.
+
+# Example 1:
+
+# Input: n = 16
+# Output: true
+
+# Example 2:
+
+# Input: n = 5
+# Output: false
+
+# Example 3:
+
+# Input: n = 1
+# Output: true
